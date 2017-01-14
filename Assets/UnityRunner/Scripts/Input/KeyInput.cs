d33186a04f999e5cdcda5f83388b9c847bb666ca
Jesse Freeman <me@jessefreeman.com>
2017-01-14 06:08:57 -0500

    Refactored project to now support a new Lua Runner and the existing Unity Runner. Also removed project's resource dependency and relinked to the Demo submodule's resource folder to keep the runner in sync with the examples.

---
+++ Assets/UnityRunner/Scripts/Input/KeyInput.cs
@@ -0,0 +1,43 @@
+﻿//  
+// Copyright (c) Jesse Freeman. All rights reserved.  
+// 
+// Licensed under the Microsoft Public License (MS-PL) License. 
+// See LICENSE file in the project root for full license information. 
+// 
+// Contributors
+// --------------------------------------------------------
+// This is the official list of Pixel Vision 8 contributors:
+//  
+// Jesse Freeman - @JesseFreeman
+// Christer Kaitila - @McFunkypants
+// Pedro Medeiros - @saint11
+// Shawn Rakowski - @shwany
+// 
+
+using PixelVisionSDK.Engine.Chips.IO.Controller;
+using UnityEngine;
+
+public class KeyInput : IKeyInput
+{
+
+    public string inputString
+    {
+        get { return Input.inputString; }
+    }
+
+    public bool GetKey(int key)
+    {
+        return Input.GetKey((KeyCode) key);
+    }
+
+    public bool GetKeyDown(int key)
+    {
+        return Input.GetKeyDown((KeyCode) key);
+    }
+
+    public bool GetKeyUp(int key)
+    {
+        return Input.GetKeyUp((KeyCode) key);
+    }
+
+}
---
+++ Assets/UnityRunner/Scripts/Input/KeyboardButtonInput.cs
@@ -0,0 +1,44 @@
+﻿//  
+// Copyright (c) Jesse Freeman. All rights reserved.  
+// 
+// Licensed under the Microsoft Public License (MS-PL) License. 
+// See LICENSE file in the project root for full license information. 
+// 
+// Contributors
+// --------------------------------------------------------
+// This is the official list of Pixel Vision 8 contributors:
+//  
+// Jesse Freeman - @JesseFreeman
+// Christer Kaitila - @McFunkypants
+// Pedro Medeiros - @saint11
+// Shawn Rakowski - @shwany
+// 
+
+using PixelVisionSDK.Engine.Chips.IO.Controller;
+using UnityEngine;
+
+/// <summary>
+///     This class helps capture keyboard input and needs to be registered with the ControllerChip.
+/// </summary>
+/// <example>
+///     controllerChip.UpdateControllerKey(0, new KeyboardButtonInput(Button.A, 120);
+/// </example>
+public class KeyboardButtonInput : ButtonState
+{
+
+    protected int keyCode;
+
+    public KeyboardButtonInput(Buttons button, int keyCode)
+    {
+        this.button = button;
+        mapping = keyCode;
+        this.keyCode = keyCode;
+    }
+
+    public override void Update(float timeDelta)
+    {
+        value = Input.GetKey((KeyCode) keyCode);
+        base.Update(timeDelta);
+    }
+
+}
