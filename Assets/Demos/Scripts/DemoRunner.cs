a84d8b1fc8f1d8f12b48cdffbe908041d7c5da44
Jesse Freeman <jessefreeman@gmail.com>
2018-02-09 10:08:55 -0500

    Cleaned up demos.

---
+++ Assets/Demos/Scripts/DemoRunner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using PixelVisionRunner.Chips;
+using PixelVisionSDK.Chips;
+using UnityEngine;
+
+// This class can be used to convert SpriteBuilder data into usable C# game data
+public class SpriteData
+{
+	public SpriteData(int width, int unique, int total, int[] spriteIDs)
+	{
+		this.spriteIDs = spriteIDs;
+		this.total = total;
+		this.unique = unique;
+		this.width = width;
+	}
+
+	public int[] spriteIDs;
+	public int total;
+	public int unique;
+	public int width;
+}
+
+public class DemoRunner : BaseRunner
+{
+	public string gameChip;
+	
+	public string path;
+	
+	public override List<string> defaultChips
+	{
+		get
+		{
+			var chips = base.defaultChips;
+
+			chips.Add(gameChip);
+			chips.Add(typeof(MusicChip).FullName);
+			chips.Add(typeof(SfxrSoundChip).FullName);
+			return chips;
+		}
+	}
+	
+	public override void Start()
+	{
+		
+		base.Start();
+			
+		ConfigureEngine();
+			
+		// Use this all of the resources that the game needs
+		LoadFromDir(Application.streamingAssetsPath + path); 
+	}
+	
+}
+
---
+++ Assets/Demos/Scripts/LuaDemoRunner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using PixelVisionRunner.Chips;
+using PixelVisionSDK.Chips;
+using UnityEngine;
+
+public class LuaDemoRunner : BaseRunner {
+	
+	private LuaService luaService;
+	public string path;
+	
+	public override List<string> defaultChips
+	{
+		get
+		{
+			var chips = base.defaultChips;
+
+			chips.Add(typeof(LuaGameChip).FullName);
+			chips.Add(typeof(MusicChip).FullName);
+			chips.Add(typeof(SfxrSoundChip).FullName);
+			return chips;
+		}
+	}
+
+	public override void Start()
+	{
+		base.Start();
+        
+		ConfigureEngine();
+
+		LoadFromDir(Application.streamingAssetsPath + path);
+	}
+
+	public override void ConfigureEngine(Dictionary<string, string> metaData = null)
+	{
+		base.ConfigureEngine(metaData);
+        
+		luaService = new LuaService();
+        
+#if !UNITY_WEBGL
+		luaService.script.Options.DebugPrint = s => Debug.Log(s);
+#endif
+
+		// Register Lua Service
+		tmpEngine.chipManager.AddService(typeof(LuaService).FullName, luaService);
+	}
+}
