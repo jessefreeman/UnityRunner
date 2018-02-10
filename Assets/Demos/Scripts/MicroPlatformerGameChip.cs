492ca525eb8ff07a6151748de07e91e63fc4339c
Jesse Freeman <jessefreeman@gmail.com>
2018-02-10 09:05:31 -0500

    Cleaning up tilemap, screen wrap and micro platformer demos.

---
+++ Assets/Demos/Scripts/MicroPlatformerGameChip.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using PixelVisionSDK.Chips;
+
+public class Player
+{
+    //velocity
+    public float dx;
+    public float dy;
+    public int spriteID = 1;
+
+    //is the player standing on
+    //the ground. used to determine
+    //if they can jump.
+    public bool isgrounded;
+
+    //how fast the player is launched
+    //into the air when jumping.
+    public float jumpvel;
+
+    //position, representing the top left of
+    //of the player sprite.
+    public int x;
+    public int y;
+
+    public Player(int x, int y, float jumpvel)
+    {
+        this.x = x;
+        this.y = y;
+        this.jumpvel = jumpvel;
+    }
+}
+
+public class MicroPlatformerGameChip : GameChip
+{
+//}
+
+    private int flag; // stores the flag globally since it's used every frame
+
+
+//globals
+//g=
+//{
+    private readonly float grav = 0.1f; // gravity per frame
+
+    /*
+    Micro Platformer - Platforming Framework in 100 lines of code.
+    Created by Matt Hughson (@matthughson | http://www.matthughson.com/)
+
+    Update to PV8 v1.5 API by Jesse Freeman (@jessefreeman | http://pixelvision8.com)
+    */
+
+    /*
+        The goal of this cart is to demonstrate a very basic
+        platforming engine in under 100 lines of *code*, while
+        still maintaining an organized and documented game. 
+    
+        It isn't meant to be a demo of doing as much as possible, in
+        as little code as possible. The 100 line limit is just 
+        meant to encourage people to realize "You can make a game
+        with very little coding!"
+    
+        This will hopefully give new users a simple and easy to 
+        understand starting point for their own platforming games.
+    
+        Note: Collision routine is based on mario bros 2 and 
+        mckids, where we use collision points rather than a box.
+        this has some interesting bugs but if it was good enough for
+        miyamoto, its good enough for me!
+    */
+
+//player information
+    protected Player p1 = new Player(72, 16, 3.0f);
+
+//called once at the start of the program.
+    public override void Init()
+    {
+        BackgroundColor(0);
+    }
+
+//called 60 times per second
+    public override void Update(float deltaTime)
+    {
+        
+        base.Update(deltaTime);
+        
+        //remember where we started
+        var startx = p1.x;
+
+        //jump 
+        //
+
+        //if on the ground and the
+        //user presses a,b,or,up...
+
+        if ((Button(Buttons.Up) || Button(Buttons.A) || Button(Buttons.B)) && p1.isgrounded) p1.dy = -p1.jumpvel;
+
+        //walk
+        //
+
+        p1.dx = 0;
+
+        if (Button(Buttons.Left)) p1.dx = -1;
+        if (Button(Buttons.Right)) p1.dx = 1;
+
+        //move the player left/right
+        p1.x = (int)(p1.x + p1.dx); // TODO should this be an int?
+
+        //hit side walls
+        //
+
+        //check for walls in the
+        //direction we are moving.
+        var xoffset = 0; //moving left check the left side of sprite.
+        if (p1.dx > 0) xoffset = 7;
+
+        //look for a wall on either the left or right of the player
+        //and at the players feet.
+        //We divide by 8 to put the location in TileMap space (rather than
+        //pixel space).
+        flag = Flag((p1.x + xoffset) / 8, (p1.y + 7) / 8);
+        //We use flag 0 (solid black) to represent solid walls. This is controlled 
+        //by tilemap-flags.png.
+        if (flag == 0) p1.x = startx;
+
+        //accumulate gravity
+        p1.dy = p1.dy + grav;
+
+        //apply gravity to the players position.
+        p1.y = (int)(p1.y + p1.dy); // TODO should this be an int?
+
+        //hit floor
+        //
+
+        //assume they are floating 
+        //until we determine otherwise
+        p1.isgrounded = false;
+
+        //only check for floors when
+        //moving downward
+        if (p1.dy >= 0)
+        {
+            //check bottom center of the
+            //player.
+            flag = Flag((p1.x + 4) / 8, (p1.y + 8) / 8);
+            //look for a solid tile
+            if (flag == 0)
+            {
+                //place p1 on top of tile
+                p1.y = (int) Math.Floor(p1.y / 8f) * 8;
+                //halt velocity
+                p1.dy = 0;
+                //allow jumping again
+                p1.isgrounded = true;
+            }
+        }
+
+        //hit ceiling
+        //
+
+        //only check for ceilings when
+        //moving up
+        if (p1.dy <= 0)
+        {
+            //check top center of player
+            flag = Flag((p1.x + 4) / 8, p1.y / 8);
+            //look for solid tile
+            if (flag == 0)
+            {
+                //position p1 right below
+                //ceiling
+                p1.y = (int) Math.Floor((p1.y + 8f) / 8) * 8;
+                //halt upward velocity
+                p1.dy = 0;
+            }
+        }
+    }
+
+    public override void Draw()
+    {
+        //clear the screen so we start each frame
+        //with a blank canvas to draw on.
+        RedrawDisplay();
+        //draw the player, represented as sprite 1.
+        DrawSprite(p1.spriteID, p1.x, p1.y); //draw player
+    }
+}
---
+++ Assets/Demos/Scripts/TilemapDemoGameChip.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using PixelVisionSDK;
+using PixelVisionSDK.Chips;
+
+internal class Skeleton
+{
+    public DrawMode aboveBG;
+    public bool flipH;
+
+    public int[] sprites;
+    public int width;
+    public int x;
+    public int y;
+
+    public Skeleton(int[] sprites, int width, bool flipH, DrawMode aboveBg, int x, int y)
+    {
+        this.sprites = sprites;
+        this.width = width;
+        this.flipH = flipH;
+        aboveBG = aboveBg;
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class TilemapDemoGameChip : GameChip
+{
+    private readonly SpriteData chest = new SpriteData(2, 4, 4, new[] {4, 5, 21, 22});
+    private readonly float delayTime = .5f;
+    private readonly Vector direction = new Vector();
+    private readonly int hudHeight = 16;
+    private readonly SpriteData skeleton = new SpriteData(2, 6, 6, new[] {2, 3, 19, 20, 31, 32});
+    private readonly int speed = 24;
+    private readonly SpriteData watertile = new SpriteData(1, 1, 1, new[] {6});
+    private int bottomBorder;
+    private Rect bounds;
+
+    private float delay = .5f;
+    private int rightBorder;
+
+    // This this is an empty game, we will the following text. We combined two sets of fonts into
+// the default.font.png. Use uppercase for larger characters and lowercase for a smaller one.
+    private float scrollX;
+    private float scrollY;
+
+    private Skeleton[] skeletons;
+    
+    private int waveMode;
+
+// The Init() method is part of the game's lifecycle and called a game starts. We are going to
+// use this method to configure background color, ScreenBufferChip and draw a text box.
+    public override void Init()
+    {
+        skeletons = new[]
+        {
+            new Skeleton(skeleton.spriteIDs, skeleton.width, false, DrawMode.Sprite, 104, 64),
+            new Skeleton(skeleton.spriteIDs, skeleton.width, false, DrawMode.SpriteBelow, 64, 94),
+            new Skeleton(skeleton.spriteIDs, skeleton.width, true, DrawMode.SpriteBelow, 120, 94),
+            new Skeleton(skeleton.spriteIDs, skeleton.width, false, DrawMode.Sprite, 180, 144),
+
+            new Skeleton(skeleton.spriteIDs, skeleton.width, false, DrawMode.Sprite, 72, 240),
+            new Skeleton(skeleton.spriteIDs, skeleton.width, false, DrawMode.Sprite, 240, 280),
+            new Skeleton(skeleton.spriteIDs, skeleton.width, false, DrawMode.Sprite, 160, 192),
+            new Skeleton(skeleton.spriteIDs, skeleton.width, false, DrawMode.Sprite, 168, 248)
+        };
+
+        // Change the background color
+        BackgroundColor(2);
+
+        // Get sprite, tilemap and display sizes
+        var spriteSize = SpriteSize();
+        var tilemapSize = TilemapSize();
+        var displaySize = Display();
+
+        // Need to get a reference to the right edge of the tilemap
+        rightBorder = tilemapSize.x * spriteSize.x - displaySize.x;
+        bottomBorder = tilemapSize.y * spriteSize.y - displaySize.y;
+
+        // Get the current visual bounds and modify for the new HUD
+        bounds = NewRect(-8, -8, displaySize.x, displaySize.y);
+        // bounds.y = 8
+
+        // Setup water tiles before r}ering map
+        var waterColumns = 6;
+        var waterRows = tilemapSize.y;
+        var totalTiles = waterColumns * waterRows;
+
+        var waterTiles = new int[totalTiles];
+        for (var i = 0; i < totalTiles; i++)
+            //TODO need to use modulus to calculate the right tiles?
+            waterTiles[i] = watertile.spriteIDs[0];
+
+        // Left water
+        UpdateTiles(0, 3, waterColumns, waterTiles, 3);
+
+        // // Right Water
+        UpdateTiles(34, 3, waterColumns, waterTiles, 3);
+
+        scrollX = 0;
+        scrollY = 0;
+    }
+
+// The Update() method is part of the game's life cycle. The engine calls Update() on every frame
+// before the Draw() method. It accepts one argument, timeDelta, which is the difference in
+// milliseconds since the last frame.
+    public override void Update(float timeDelta)
+    {
+        base.Update(timeDelta);
+
+        scrollX = scrollX + speed * timeDelta * direction.x;
+        scrollY = scrollY + speed * timeDelta * direction.y;
+
+        if (scrollX >= rightBorder)
+        {
+            scrollX = rightBorder;
+            direction.x = -1;
+        }
+        else if (scrollX <= 0)
+        {
+            scrollX = 0;
+            direction.x = 1;
+        }
+
+        if (scrollY >= bottomBorder)
+        {
+            scrollY = bottomBorder;
+            direction.y = -1;
+        }
+        else if (scrollY <= hudHeight)
+        {
+            scrollY = hudHeight;
+            direction.y = 1;
+        }
+
+        // We start by adding the time delta to the delay.
+        delay = delay + timeDelta;
+
+        // Next, we will need to test if the delay value is greater than the delayTime field we set up at the
+        // beginning of our class.
+        if (delay > delayTime)
+        {
+            if (waveMode == 0)
+            {
+                waveMode = 1;
+                ReplaceColor(4, 2);
+                ReplaceColor(6, 1);
+            }
+            else if (waveMode == 1)
+            {
+                waveMode = 0;
+                ReplaceColor(4, 1);
+                ReplaceColor(6, 2);
+            }
+
+            // We need to reset the delay so we can start tracking it again on the next frame.
+            delay = 0;
+        }
+    }
+
+// The Draw() method is part of the game's life cycle. It is called after Update() and is where
+// all of our draw calls should go. We'll be using this to r}er sprites to the display.
+    public override void Draw()
+    {
+        Clear();
+
+        // Convert the scrollX value into a whole number
+        var newScrollX = (int) Math.Floor(scrollX);
+        var newScrollY = (int) Math.Floor(scrollY);
+
+        // scroll the tilemap map down below the HUD which is 16 pixels hight. Also apply the new scrollX value
+        ScrollPosition(newScrollX, newScrollY);
+
+        DrawTilemap(0, hudHeight, 20, 15, newScrollX, newScrollY + hudHeight);
+
+        // Draw sprites
+
+        var total = skeletons.Length;
+
+        for (var i = 0; i < total; i++)
+        {
+            var skeleton = skeletons[i];
+            DrawSprites(skeleton.sprites, skeleton.x, skeleton.y, skeleton.width, skeleton.flipH, false,
+                skeleton.aboveBG, 0, true, true, bounds);
+        }
+
+        DrawSprites(chest.spriteIDs, 168, 72 + 32, chest.width, false, false, DrawMode.Sprite, 0, true, true, bounds);
+
+        // Read the current scroll position from the display
+        var pos = ScrollPosition();
+
+        // Draw the scroll x and y position to the display
+        DrawText(string.Format("({0:D3},{1:D3})", pos.x, pos.y), 8, 124, DrawMode.SpriteAbove, "default");
+
+        // Need to rest tiles under tilemap cache to force it to clear correctly and maintain the HUD bg color
+        DrawTiles(new[] {17, 17, 17}, 12, 1, 3);
+        DrawTiles(new[] {17, 17}, 16, 1, 2);
+
+        // Draw new text on top of the tilemap data cache so we can maintain the transparency
+        DrawText(ReadTotalSprites().ToString(), 12 * 8, 8, DrawMode.TilemapCache, "default");
+        DrawText(ReadFPS().ToString(), 16 * 8, 8, DrawMode.TilemapCache, "default");
+
+        // Draw the HUD layer after we update the tilemap
+        DrawTilemap(0, 0, 20, 3, 0, 0, DrawMode.UI);
+    }
+}
